fb4f841420381df2ddefab39d9475bd77f109118
Kenneth Brown <siegel.zero@gmail.com>
2015-03-16 23:19:35 -0700

    Code for hw5

--- mlclass-ex5-008/mlclass-ex5/linearRegCostFunction.m
+++ mlclass-ex5-008/mlclass-ex5/linearRegCostFunction.m
@@ -1,37 +1,34 @@
 function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
-%LINEARREGCOSTFUNCTION Compute cost and gradient for regularized linear 
-%regression with multiple variables
-%   [J, grad] = LINEARREGCOSTFUNCTION(X, y, theta, lambda) computes the 
-%   cost of using theta as the parameter for linear regression to fit the 
-%   data points in X and y. Returns the cost in J and the gradient in grad
+    %LINEARREGCOSTFUNCTION Compute cost and gradient for regularized linear 
+    %regression with multiple variables
+    %   [J, grad] = LINEARREGCOSTFUNCTION(X, y, theta, lambda) computes the 
+    %   cost of using theta as the parameter for linear regression to fit the 
+    %   data points in X and y. Returns the cost in J and the gradient in grad
 
-% Initialize some useful values
-m = length(y); % number of training examples
+    % Initialize some useful values
+    m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+    % You need to return the following variables correctly 
+    J = 0;
+    grad = zeros(size(theta));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost and gradient of regularized linear 
-%               regression for a particular choice of theta.
-%
-%               You should set J to the cost and grad to the gradient.
-%
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Compute the cost and gradient of regularized linear 
+    %               regression for a particular choice of theta.
+    %
+    %               You should set J to the cost and grad to the gradient.
+    %
 
-
-
-
-
-
+    J1 = sum((X*theta - y).^2)/(2*m);
+    J2 = sum(dot(theta(2:end), theta(2:end)))*(lambda/(2*m));
+    J = J1 + J2;
 
 
+    % =========================================================================
 
+    grad(1) = dot(X*theta - y, X(:, 1))/m;
 
-
-
-% =========================================================================
-
-grad = grad(:);
-
+    for j = 2:size(theta)
+        grad(j) = dot(X*theta - y, X(:, j))/m + theta(j)*(lambda/m);
+    end
 end
