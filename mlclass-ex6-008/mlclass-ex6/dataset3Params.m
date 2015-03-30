5b005dff2b54725fa59a7458ba26d876ba8060e2
Kenneth Brown <siegel.zero@gmail.com>
2015-03-29 22:16:47 -0700

    Added code for hw6

--- mlclass-ex6-008/mlclass-ex6/dataset3Params.m
+++ mlclass-ex6-008/mlclass-ex6/dataset3Params.m
@@ -1,34 +1,44 @@
 function [C, sigma] = dataset3Params(X, y, Xval, yval)
-%EX6PARAMS returns your choice of C and sigma for Part 3 of the exercise
-%where you select the optimal (C, sigma) learning parameters to use for SVM
-%with RBF kernel
-%   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and 
-%   sigma. You should complete this function to return the optimal C and 
-%   sigma based on a cross-validation set.
-%
+    %EX6PARAMS returns your choice of C and sigma for Part 3 of the exercise
+    %where you select the optimal (C, sigma) learning parameters to use for SVM
+    %with RBF kernel
+    %   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and 
+    %   sigma. You should complete this function to return the optimal C and 
+    %   sigma based on a cross-validation set.
+    %
 
-% You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
+    % You need to return the following variables correctly.
+    C = 1;
+    sigma = 0.1;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return the optimal C and sigma
-%               learning parameters found using the cross validation set.
-%               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
-%                   predictions = svmPredict(model, Xval);
-%               will return the predictions on the cross validation set.
-%
-%  Note: You can compute the prediction error using 
-%        mean(double(predictions ~= yval))
-%
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Fill in this function to return the optimal C and sigma
+    %               learning parameters found using the cross validation set.
+    %               You can use svmPredict to predict the labels on the cross
+    %               validation set. For example, 
+    %                   predictions = svmPredict(model, Xval);
+    %               will return the predictions on the cross validation set.
+    %
+    %  Note: You can compute the prediction error using 
+    %        mean(double(predictions ~= yval))
+    %
+    % Uncomment the following code to return optimal C and sigma values.
+    %values = [0.01 0.03 0.1 0.3 1 3 10 30];
+    %min_err = 10000;
 
-
-
-
-
-
-
-% =========================================================================
-
+    %for C = values
+    %    for sigma = values
+    %        model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+    %        predictions = svmPredict(model, Xval);
+    %        err = mean(double(predictions ~= yval));
+    %        if err < min_err
+    %            min_err = err;
+    %            min_C = C;
+    %            min_sigma = sigma;
+    %        end
+    %    end
+    %end
+    %C = min_C
+    %sigma = min_sigma
+    % =========================================================================
 end
